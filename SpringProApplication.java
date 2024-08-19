cc1f596ddb163a7038cca6849632ee1bb6f8a080
Sushmitha Paramasivam <136717568+Sushmitha1710@users.noreply.github.com>
2024-08-19 13:37:32 +0530

    Add files via upload

---
+++ SpringProApplication.java
@@ -0,0 +1,16 @@
+package com.example.SpringPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringProApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringProApplication.class, args);
+	}
+
+}
